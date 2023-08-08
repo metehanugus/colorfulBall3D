6e80686f2b51934439de41cb970eb5e35b2fad59
metehanugus <65510727+metehanugus@users.noreply.github.com>
2023-08-08 20:07:13 +0300

    update2
    Assetler eklendi, dokunmatik kontrol scripti (velocity+rb) eklendi, test objeleri grounda eklendi

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Touch touch;
+    public int speedModifier;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                rb.velocity = new Vector3(touch.deltaPosition.x * speedModifier * Time.deltaTime,
+                                          transform.position.y,
+                                          touch.deltaPosition.y * speedModifier * Time.deltaTime);
+            }
+        }
+    }
+}
