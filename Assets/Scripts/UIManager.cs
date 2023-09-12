9ec6b6f99374a4620835aec1294afa11a44d02b9
metehanugus <65510727+metehanugus@users.noreply.github.com>
2023-09-12 21:49:23 +0300

    update10
    settings + animasyonları eklendi.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,6 +7,62 @@ public class UIManager : MonoBehaviour
 {
     public Image whiteeffectimage;
     private int effectcontrol = 0;
+
+
+    public Animator LayoutAnimator;
+
+    //Butonlar
+    public GameObject settings_open;
+    public GameObject settings_close;
+    public GameObject sound_on;
+    public GameObject sound_off;
+    public GameObject vibration_on;
+    public GameObject vibration_off;
+    public GameObject iap;
+    public GameObject information;
+
+    //Buton Fonksiyonlari
+
+    public void Settings_Open()
+    {
+        settings_open.SetActive(false);
+        settings_close.SetActive(true);
+        LayoutAnimator.SetTrigger("slide_in");
+    }
+
+    public void Settings_Close()
+    {
+        settings_close.SetActive(false);
+        settings_open.SetActive(true);
+        LayoutAnimator.SetTrigger("slide_out");
+    }
+
+    public void Sound_On ()
+    {
+        sound_on.SetActive(false);
+        sound_off.SetActive(true);
+    }
+
+    public void Sound_Off()
+    {
+        sound_off.SetActive(false);
+        sound_on.SetActive(true);
+    }
+
+    public void Vibration_On ()
+    {
+        vibration_on.SetActive(false);
+        vibration_off.SetActive(true);
+    }
+
+    public void Vibration_Off ()
+    {
+        vibration_off.SetActive(false);
+        vibration_on.SetActive(true) ;
+    }
+
+
+
     public IEnumerator WhiteEffect()
     {
         whiteeffectimage.gameObject.SetActive(true);
